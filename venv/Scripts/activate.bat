56aa46c98e77b31d7073dc9b48bcf0857c076663
kamna <kamna.dwivedi.24@gmail.com>
2025-11-21 11:05:19 +0530

    Add build.sh for Render

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\kamna\OneDrive\Desktop\VERDANTIS-BACKEND\venv
+set VIRTUAL_ENV=C:\Dev_projects\VERDANTIS-BACKEND\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
